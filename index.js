2f992b866d9d0cdc180963aacce5d7514a157c96
diyana <labibadiyana100@gmail.com>
2023-05-30 16:43:32 +0600

    checking

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+  }));
 app.use(express.json());
 
 
